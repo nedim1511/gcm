61e20e6ce61d9c139631e137bb86c4874b6f5c15
Patrick Giedemann <patrick.giedemann@sintratec.com>
2021-01-27 13:14:28 +0100

    update backend api url constant to match latest mock server

--- src/app/shared/constants.ts
+++ src/app/shared/constants.ts
@@ -1,2 +1,3 @@
-export const BACKEND_API_URL = "https://virtserver.swaggerhub.com/ZWEIMALL/GCM-API/1.0.0";
+// export const BACKEND_API_URL = "https://virtserver.swaggerhub.com/ZWEIMALL/GCM-API/1.0.0";
+export const BACKEND_API_URL = "https://09d54c99-0a15-4908-96e4-60fba2942a7b.mock.pstmn.io";
 export const AUTH_API_URL = "http://gcm.eu.auth0.com/";
