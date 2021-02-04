d2c05303c46c2b9738b0dd7eb6ab609d97dfd845
Nedim <kurbegovicnedim@gmail.com>
2021-02-04 15:04:31 +0100

    implement modals on redeem and topup

--- src/app/tab4/card-detail/redeem-card/redeem-card.component.ts
+++ src/app/tab4/card-detail/redeem-card/redeem-card.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, Router } from "@angular/router";
 import { CardListItem } from "src/app/models/card-list-item.model";
 import { Tab2Service } from "src/app/services/tab2.service";
 import { Storage } from "@ionic/storage";
+import { AlertController } from "@ionic/angular";
 
 @Component({
   selector: "app-redeem-card",
@@ -18,7 +19,8 @@ export class RedeemCardComponent implements OnInit {
     private service: Tab2Service,
     private route: ActivatedRoute,
     private router: Router,
-    private storage: Storage
+    private storage: Storage,
+    public alertController: AlertController
   ) {
     this.amount = 0;
     this.get();
@@ -33,6 +35,7 @@ export class RedeemCardComponent implements OnInit {
   }
 
   redeem(max?: boolean) {
+    this.amount = max ? this.item.amount : this.amount;
     if (this.amount > this.item.amount) {
       this.amount = 0;
       alert("Redeem amount cannot be higher than current amount.");
@@ -46,9 +49,44 @@ export class RedeemCardComponent implements OnInit {
       alert("Comment length cannot exceed 1000 characters.");
       return;
     }
-    this.amount = max ? this.item.amount : this.amount;
-    this.storage.set("redeemAmount", this.amount);
-    this.storage.set("redeemComment", this.comment);
-    this.router.navigate(["confirm"], { relativeTo: this.route });
+    this.confirmRedeem();
+    // this.storage.set("redeemAmount", this.amount);
+    // this.storage.set("redeemComment", this.comment);
+    // this.router.navigate(["confirm"], { relativeTo: this.route });
+  }
+
+  async confirmRedeem() {
+    const alert = await this.alertController.create({
+      header: "Confirm",
+      message:
+        "Are you sure you want to proceed? The new amount will be <strong>" +
+        this.item.currency +
+        " " +
+        (this.item.amount - this.amount) +
+        "</strong>." +
+        (this.comment
+          ? "<br><br><strong>Comment: </strong>" + this.comment
+          : ""),
+      buttons: [
+        {
+          text: "Cancel",
+          role: "cancel",
+          cssClass: "secondary",
+          handler: () => {
+            console.log("Confirm Cancel");
+          },
+        },
+        {
+          text: "Yes",
+          handler: () => {
+            this.service.redeem(this.item.id, this.amount).subscribe(() => {
+              this.router.navigate(["/tabs/tab4/details/" + this.item.id]);
+            });
+          },
+        },
+      ],
+    });
+
+    await alert.present();
   }
 }
--- src/app/tab4/card-detail/top-up-card/top-up-card.component.ts
+++ src/app/tab4/card-detail/top-up-card/top-up-card.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, Router } from "@angular/router";
 import { CardListItem } from "src/app/models/card-list-item.model";
 import { Tab2Service } from "src/app/services/tab2.service";
 import { Storage } from "@ionic/storage";
+import { AlertController } from "@ionic/angular";
 
 @Component({
   selector: "app-top-up-card",
@@ -19,7 +20,8 @@ export class TopUpCardComponent implements OnInit {
     private service: Tab2Service,
     private route: ActivatedRoute,
     private router: Router,
-    private storage: Storage
+    private storage: Storage,
+    public alertController: AlertController
   ) {
     this.amount = 0;
     this.get();
@@ -42,11 +44,60 @@ export class TopUpCardComponent implements OnInit {
       alert("Top up amount must be greater than 0.");
       return;
     }
-    this.storage.set(
-      "topUpAmount",
-      this.segment === "tu" ? (this.item.amount += +this.amount) : +this.amount
-    );
-    this.storage.set("topUpComment", this.comment);
-    this.router.navigate(["confirm"], { relativeTo: this.route });
+    if (this.segment !== "tu" && this.amount <= this.item.amount) {
+      alert("This amount has to be larger than the current amount");
+      return;
+    }
+    this.confirmTopUp();
+    // this.storage.set(
+    //   "topUpAmount",
+    //   this.segment === "tu" ? (this.item.amount += +this.amount) : +this.amount
+    // );
+    // this.storage.set("topUpComment", this.comment);
+    // this.router.navigate(["confirm"], { relativeTo: this.route });
+  }
+
+  async confirmTopUp() {
+    const alert = await this.alertController.create({
+      header: "Confirm",
+      message:
+        "Are you sure you want to proceed? The new amount will be <strong>" +
+        this.item.currency +
+        " " +
+        (this.segment === "tu"
+          ? (this.item.amount + +this.amount)
+          : +this.amount) +
+        "</strong>." +
+        (this.comment
+          ? "<br><br><strong>Comment: </strong>" + this.comment
+          : ""),
+      buttons: [
+        {
+          text: "Cancel",
+          role: "cancel",
+          cssClass: "secondary",
+          handler: () => {
+            console.log("Confirm Cancel");
+          },
+        },
+        {
+          text: "Yes",
+          handler: () => {
+            this.service
+              .topUp(
+                this.item.id,
+                this.segment === "tu"
+                  ? this.amount
+                  : (this.amount - this.item.amount)
+              )
+              .subscribe(() => {
+                this.router.navigate(["/tabs/tab4/details/" + this.item.id]);
+              });
+          },
+        },
+      ],
+    });
+
+    await alert.present();
   }
 }
