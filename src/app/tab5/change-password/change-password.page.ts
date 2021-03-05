7d5f92b0c93a861562f3745bf03332b2e295bb73
Nedim <kurbegovicnedim@gmail.com>
2021-03-05 21:46:21 +0100

    add change password page

--- src/app/tab5/change-password/change-password.page.ts
+++ src/app/tab5/change-password/change-password.page.ts
@@ -7,9 +7,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChangePasswordPage implements OnInit {
 
+  password: string;
+  confirm: string;
+  old: string;
+
   constructor() { }
 
+  save() {
+    if (this.isValid()) {
+      alert('Success. Now goes backend call');
+    }
+  }
+
   ngOnInit() {
+  }
+
+  private isValid() {
+    if (!this.old) {
+      alert('Old password is required.');
+      return false;
+    }
+    if (!this.password) {
+      alert('New password is required.');
+      return false;
+    }
+    if (!this.confirm) {
+      alert('Confirm password is required.');
+      return false;
+    }
+    if (this.confirm !== this.password) {
+      alert('New passwords are not the same.');
+      return false;
+    }
+    if (this.password === this.old) {
+      alert('Please use a different password than your current one.');
+      return false;
+    }
+    return true;
   }
 
 }
